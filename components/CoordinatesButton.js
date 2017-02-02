30483fd31aaae2af35b37cc68b7e3dc4109264e5
wongjenn <wongjenn@yahoo.com>
2017-02-02 09:11:19 -0500

    Done.

---
+++ components/CoordinatesButton.js
@@ -0,0 +1,22 @@
+const React = require('react')
+
+class CoordinatesButton extends React.Component {
+  constructor(){
+    super()
+    this.handleClick = this.handleClick.bind(this)
+
+  }
+
+  handleClick(event){
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleClick }>Click here!</button>
+    )
+  }
+
+}
+
+module.exports = CoordinatesButton
---
+++ components/DelayedButton.js
@@ -0,0 +1,22 @@
+const React = require('react')
+
+class DelayedButton extends React.Component{
+  constructor(){
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event){
+    event.persist()
+    setTimeout(this.props.onDelayedClick , this.props.delay, event)
+  }
+
+  render(){
+    return(
+      <button onClick={ this.handleClick}></button>
+    )
+  }
+
+}
+
+module.exports = DelayedButton
